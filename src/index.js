e46a7b62bf5536d427ad20341824ae63e61c316c
Carlos J Herrera H <cjherrerah@gmail.com>
2022-08-10 21:25:18 -0500

    Segundo commit incluyendo bootstrap 5, index.html y registro.html

--- src/index.js
+++ src/index.js
@@ -13,3 +13,24 @@ if(container_toogle!==null){
     
     })
 }
+
+const btnAdd =  document.getElementById("btnAdd");
+const cartFront = document.getElementById("card-front");
+cartFront.addEventListener("mousemove", ()=>{
+    console.log("ingresa al mouseover");
+    btnAdd.style.visibility = "initial";
+    // btnAdd.classList.toggle("")
+})
+
+cartFront.removeEventListener("mouseover",()=>{
+    console.log("Se remueve el listener");
+});
+
+cartFront.addEventListener("mouseleave", ()=>{
+    console.log("ingresa al mouseleave");
+    btnAdd.style.visibility = "hidden"
+})
+
+cartFront.removeEventListener("mouseleave",()=>{
+    console.log("Se remueve el listener");
+});
